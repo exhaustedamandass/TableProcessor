9aaed28e314f0b929f85de10bc181054022bda30
Assanali Amandykov <amandass@fit.cvut.cz>
2024-12-20 16:40:51 +0100

    minor adjustment

--- src/main/scala/operator/arithmeticOperators/DivOperator.scala
+++ src/main/scala/operator/arithmeticOperators/DivOperator.scala
@@ -3,6 +3,11 @@ package operator.arithmeticOperators
 import operator.ArithmeticOperator
 
 object DivOperator extends ArithmeticOperator {
-  def apply(lhs: Double, rhs: Double): Double = lhs / rhs
+  def apply(lhs: Double, rhs: Double): Double = {
+    if (rhs == 0.0) {
+      throw new ArithmeticException("Division by zero")
+    }
+    lhs / rhs
+  }
   def precedence: Int = 2
 }
